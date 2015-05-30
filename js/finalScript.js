fd649148d4dd0e987bf7167e055d20ba8f031027
Xiaohan Zeng <zengx@onid.oregonstate.edu>
2015-05-30 13:03:20 -0700

    first commit

---
+++ js/finalScript.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+		$("#dialog").dialog({
+		autoOpen: false,
+		show: 'slide',
+		resizable: false,
+		stack: true,
+		height: '400',
+		width: '400px'
+	});		
+});
+					
+function popWindow(){
+	
+	$("#dialog").dialog('open');
+}
