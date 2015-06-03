65881dd1780a55cdf4073119f88412e7dc33698a
xiaohan on home pc <zengx@onid.oregonstate.edu>
2015-06-02 23:21:03 -0700

    get trending stories done. Added cache

--- js/trendingItemCrawler.js
+++ js/trendingItemCrawler.js
@@ -4,104 +4,11 @@ function populateTrendingPictures(){
 	$.ajax({
 	method: "GET",
     url: "php/googleTrend.php",
-    dataType: "json",
+    dataType: "html",
     success: function (returndata)
     {
-		for (i=0; i<returndata.length; i++) {
-			var url = "https://www.googleapis.com/customsearch/v1?cx=007306240209605891397%3Ah9qucp1qv8g&q=" + encodeURIComponent(returndata[i]) + "&num=3&key=AIzaSyDqHSQNdJfxBFIvTmwHfpBR2HWo4FZ7aOI";
-			
-			getImageLink(url, returndata[i], i);
-		}
+		document.getElementById('photoContainer').innerHTML = returndata;
     }  
 });
 }
 
-function getImageLink(url, title, index){
-	$.ajax({
-	method: "GET",
-    url: url,
-	title: title,
-	index: index,
-    dataType: "json",
-    success: function (returndata)
-    {
-		var item = findValue('cse_thumbnail', returndata);
-		var imgUrl = findValue('src', item);
-		var imgPageLink = findValue('link', returndata);
-		if (imgUrl !== null &&  imgPageLink!== null)
-		{
-			var item = constructNewPhotoItem(this.title, imgUrl, imgPageLink);
-			var rows = document.getElementById('photoContainer').getElementsByClassName("row");
-			var lastRow = rows[rows.length-1];
-			if (lastRow == null || lastRow.children.length >= 3)
-			{
-				var new_row = document.createElement( 'div' );
-				new_row.className = "row";
-				document.getElementById('photoContainer').appendChild(new_row);
-				lastRow = new_row
-			}
-			lastRow.appendChild(item);
-			console.log(this.title);
-			console.log(imgUrl);
-			console.log(imgPageLink);
-		}
-    }  
-});
-
-// Construct a div that contains a photo
-function constructNewPhotoItem(title, imgUrl, imgPageLink)
-{
-	var new_item = document.createElement( 'div' );
-	new_item.className = "col-md-4 portfolio-item";
-	
-	var photo_div = document.createElement( 'a' );
-	photo_div.href = imgPageLink;
-	var img = document.createElement( 'img' );
-	img.className = "img-responsive";
-	img.src = imgUrl;
-	photo_div.appendChild(img);
-	new_item.appendChild(photo_div);
-	
-	var title_container = document.createElement( 'h3' );
-	var title_link = document.createElement( 'a' );
-	title_link.href = imgPageLink;
-	title_link.title = title;
-	var titleText = document.createTextNode(title);
-	title_link.appendChild(titleText);
-	title_container.appendChild(title_link);
-	new_item.appendChild(title_container);
-	
-	var pin_link = document.createElement( 'a' );
-	pin_link.className = "floatMenu";
-	pin_link.onclick = "popWindow()";
-	var pinText = document.createTextNode("click to Pin");
-	pin_link.appendChild(pinText);
-	pin_link.title = "click to Pin";
-	new_item.appendChild(pin_link);
-	
-	return new_item;
-	
-}
-
-function findValue(needle, array) { 
-    var value = null;
-
-    for (var key in array) {
-		
-		if (key == needle) {
-            return array[key];
-        }
-		else if (typeof array[key] == "object"){	
-			value = findValue(needle, array[key]);
-			if (value !== null)
-			{
-				return value;
-			}
-		}
-    }
-
-    return value;    
-} 
-
-
-}
--- php/googleTrend.php
+++ php/googleTrend.php
@@ -1,53 +1,151 @@
 <?php
+date_default_timezone_set('America/Los_Angeles');
 
-$google_trend_data = array(
+$googleTrendData = array(
 	'pn' => 'p1',
 	'ajax' => 1,
 	'htv' => 'l',
 	'htd' => date("Ymd")
 );
 
-$google_trend_url = 'http://www.google.com/trends/hottrends/hotItems';
-	
+$googleTrendUrl = 'http://www.google.com/trends/hottrends/hotItems';
+
+$cacheFilePath = "cached_data.txt";
+
 function getTrendingStories()
 {
-	global $google_trend_data, $google_trend_url;
+	global $googleTrendData, $googleTrendUrl;
 	
-	// Make a request to google trend to get today's trending stories
-	$resp = makeRequest($google_trend_url, "POST", $google_trend_data);
-	
-	// Decode json string and find out titles of trending stories
-	$raw_json = json_decode($resp, true);
-	$trend_lists = recursiveFind($raw_json, "trendsList");
-	$trend_title_lists = array();	
-	foreach($trend_lists[0] as $value)
+	$cachedData = getCachedData();
+	if ($cachedData == null)
 	{
-		//var_dump($value);
-		$trend_title_lists[] = $value['title'];
-	}
+		// Make a request to google trend to get today's trending stories
+		$resp = makeRequest($googleTrendUrl, "POST", $googleTrendData);
+		
+		// Decode json string and find out titles of trending stories
+		$rawJson = json_decode($resp, true);
+		$trendLists = recursiveFind($rawJson, "trendsList");
+		$trendTitleLists = array();	
+		foreach($trendLists[0] as $value)
+		{
+			$trendTitleLists[] = $value['title'];
+		}
 
-	// Slice the array to only get top 30 titles
-	$trend_title_lists = array_slice($trend_title_lists, 0, 3);
-	
-	// Encode the results array to JSON string
-	$trend_lists_encoded = json_encode(array_values($trend_title_lists));
-	
-	echo $trend_lists_encoded;
+		// Slice the array to only get top 30 titles
+		$trendTitleLists = array_slice($trendTitleLists, 0, 3);
+		
+		$count = 0;			
+		$dom = new DOMDocument;
+		$container = $dom->createElement('div');
+		$rowContainer = NULL;
+		foreach($trendTitleLists as $value)
+		{
+			$imageItem = getImageTitleAndLink($value);
+			$imageContainer = constructNewPhotoNode($dom, $imageItem);			
+			if ($count % 3 == 0)
+			{
+				$rowContainer = $dom->createElement('div');
+				$rowContainer->setAttribute("class", "row");
+				$container->appendChild($rowContainer);
+			}
+			$rowContainer->appendChild($imageContainer);
+			$count++;			
+		}
+		$dom->appendChild($container);
+		
+		writeCachedData($dom->saveHTML($container));
+		echo $dom->saveHTML($container);
+	}
+	else
+	{
+		echo $cachedData;
+	}
 }
 
 getTrendingStories();
 
-
-
 // ------------------ helper functions -----------------------------
 
 /***************************************************
-	Make POST request using curl and return the response 
-	param url: url to send the request to
-	param data: data to include in the POST request. Needs to be a dictionary which contains key value
-	pairs for each parameter 
-	param requestType: string that represents request type, it needs to be either GET or POST, default to GET
-	return: response from server
+Construct a dom element that represents an image item
+param $imageItem: an object that has title, imgUrl, imgPageLink properties
+****************************************************/
+function constructNewPhotoNode($dom, $imageItem)
+{
+	$imageContainer = $dom->createElement('div');
+	$imageContainer->setAttribute("class", "col-md-4 portfolio-item");
+	
+	$imageLink = $dom->createElement('a');
+	$imageLink->setAttribute("href", "$imageItem->imgPageLink");
+	$imageContainer->appendChild($imageLink);
+	
+	$img = $dom->createElement('img');
+	$img->setAttribute("class", "img-responsive");
+	$img->setAttribute("src", "$imageItem->imgUrl");
+	$imageLink->appendChild($img);
+	
+	$imgTitleContainer = $dom->createElement('h3');
+	$imgTitle = $dom->createElement('a', "$imageItem->title");
+	$imgTitle->setAttribute("href", "$imageItem->imgPageLink");
+	$imgTitleContainer->appendChild($imgTitle);
+	$imageContainer->appendChild($imgTitleContainer);
+	
+	$pinLink = $dom->createElement('a', "Click to Pin");
+	$pinLink->setAttribute("class", "floatMenu");
+	$pinLink->setAttribute("onclick", "popWindow()");
+	$pinLink->setAttribute("href", "#");
+	$imageContainer->appendChild($pinLink);
+
+	return $imageContainer;
+}
+
+
+/***************************************************
+Given an array and return first item that is not NULL or empty
+****************************************************/
+function getValidItem($array)
+{
+	foreach($array as $item)
+	{
+		if($item != NULL && !empty($item))
+		{
+			return $item;
+		}
+	}
+}
+
+/***************************************************
+Given a keyword and use google custom API to search 
+and parse the returned data to get link of image thumbnail
+and page link
+param title: keyword to search
+return: an object that consists of keyword, image thumbnail's link and page link
+****************************************************/
+function getImageTitleAndLink($title)
+{
+	$url = "https://www.googleapis.com/customsearch/v1?cx=007306240209605891397%3Ah9qucp1qv8g&q=".urlencode($title)."&num=5&key=AIzaSyDqHSQNdJfxBFIvTmwHfpBR2HWo4FZ7aOI";
+	$resp = makeRequest($url, "GET");
+	$rawJson = json_decode($resp, true);
+	
+	$thumbnails = recursiveFind($rawJson, "pagemap", ["cse_thumbnail"]);
+	$thumbnailLinks = recursiveFind($thumbnails, "src");
+	$pageLinks = recursiveFind($rawJson, "link");
+	$thumbnailLink = getValidItem($thumbnailLinks);
+	$pageLink = getValidItem($pageLinks);
+	$imageItem = (object) array('title' => $title,
+								'imgUrl' => $thumbnailLink,
+								'imgPageLink' => $pageLink);
+	
+	return $imageItem;
+}
+
+/***************************************************
+Make POST request using curl and return the response 
+param url: url to send the request to
+param data: data to include in the POST request. Needs to be a dictionary which contains key value
+pairs for each parameter 
+param requestType: string that represents request type, it needs to be either GET or POST, default to GET
+return: response from server
 ****************************************************/
 function makeRequest($url, $requestType, $data = NULL)
 {
@@ -63,7 +161,8 @@ function makeRequest($url, $requestType, $data = NULL)
 		curl_setopt($curl, CURLOPT_POST, 1);
 		curl_setopt($curl, CURLOPT_POSTFIELDS, $postString);
 	}
-
+	
+	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 	
 	// Send the request & save response to $resp
@@ -75,15 +174,14 @@ function makeRequest($url, $requestType, $data = NULL)
 	
 	// Close request to clear up some resources
 	curl_close($curl);
-	
 	return $resp;
 }
 
 /****************************************************
-	Recursively search array for given key, for each matched key parse its value and add it to an array then return it at the end 
-	param array: array to search
-	param expected_key: keyword to search
-	param value_keys: An array that contains list of keywords to filter $value. For each found key's $value, optionally we can filter out the info in the $value that we need. Ie, if $value for a given key is an array that contains {'name':'John', 'age':25, 'height':'180cm'}. We can filter out only name and age by setting $value_keys to be {'name', 'age'}  
+Recursively search array for given key, for each matched key parse its value and add it to an array then return it at the end 
+param array: array to search
+param expected_key: keyword to search
+param value_keys: An array that contains list of keywords to filter $value. For each found key's $value, optionally we can filter out the info in the $value that we need. Ie, if $value for a given key is an array that contains {'name':'John', 'age':25, 'height':'180cm'}. We can filter out only name and age by setting $value_keys to be {'name', 'age'}  
 ****************************************************/
 function recursiveFind(array $array, $expected_key, $value_keys = NULL)
 {
@@ -111,6 +209,44 @@ function recursiveFind(array $array, $expected_key, $value_keys = NULL)
     }
 	
 	return $results;
+}
+
+// simple implementation for caching. This might cause trouble if there are lots of concurrent request
+
+/****************************************************
+Get cached data from file, return null if file doesn't exist
+or cached data has expired(cache expires every calendar day)
+****************************************************/
+function getCachedData()
+{
+	global $cacheFilePath;
+	if (!file_exists($cacheFilePath))
+		return null;
+	$lines = file($cacheFilePath);
+	$date = $lines[0];
+	$date = preg_replace('/([\r\n\t])/','', $date); // remove EOL
+	if ($date == date("Ymd"))
+	{	
+		$trending_data = $lines[1];
+		$trending_data = preg_replace('/([\r\n\t])/','', $trending_data); // remove EOL
+		return $trending_data;
+	}
+	else
+	{
+		return null;
+	}
+}
+
+/****************************************************
+Write cached data to file, first line will be date on 
+which the cached is written
+****************************************************/
+function writeCachedData($data)
+{
+	global $cacheFilePath;
+	$data = preg_replace('/([\r\n\t])/','', $data); // remove EOL
+	file_put_contents($cacheFilePath, date("Ymd").PHP_EOL );
+	file_put_contents($cacheFilePath, $data.PHP_EOL , FILE_APPEND);
 }
 
 ?>
